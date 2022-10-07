75735b0bb25d0222adf05a4dc00f9655beba0c7b
Nacho <nachho.losa@gmail.com>
2022-10-06 23:39:03 -0300

    add Custom Hook -useRepositories

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import { Text, FlatList } from "react-native";
-import repositories from "../data/repositories.js";
 import RepositoryItem from "./RepositoryItem.jsx";
+import useRepositories from "../hooks/useRepositories.js";
 
 const RepositoryList = () => {
+	const { repositories } = useRepositories();
+
 	return (
 		<>
+			{console.log("aloha")}
 			<FlatList
 				data={repositories}
 				ItemSeparatorComponent={() => <Text></Text>}
 				renderItem={({ item: repo }) => <RepositoryItem {...repo} />}
 			/>
-			<Text>Hello world</Text>
+			<Text>Aloha</Text>
 		</>
 	);
 };
---
+++ src/hooks/useRepositories.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+
+const useRepositories = () => {
+	const [repositories, setRepositories] = useState(null);
+
+	const fetchRepositories = async () => {
+		const response = await globalThis.fetch(
+			"http://192.168.0.166:5000/api/repositories"
+		);
+		const json = await response.json();
+		setRepositories(json);
+	};
+
+	useEffect(() => {
+		fetchRepositories();
+	}, []);
+
+	const repositoriesNodes = repositories
+		? repositories.edges.map(
+				(edge) => edge.node,
+				console.log("hola toy mapeando")
+		  )
+		: [];
+
+	return { repositories: repositoriesNodes };
+};
+
+export default useRepositories;
